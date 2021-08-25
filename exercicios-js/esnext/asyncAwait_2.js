66b347d4e0bfa494ad0868e5f56e458d55181b00
Silvio Silva <silvioedu@gmail.com.br>
2021-08-25 12:00:00 -0300

    finalização do capítulo esnext

---
+++ exercicios-js/esnext/asyncAwait_2.js
@@ -0,0 +1,29 @@
+function gerarNumerosEntre(min, max, numerosProibidos) {
+    if (min > max) [min, max] = [max, min]
+
+    return new Promise((resolve, reject) => {
+        const fator = max - min + 1
+        const aleatorio = parseInt(Math.random() * fator) + min
+        if(numerosProibidos.includes(aleatorio)) {
+            reject('Número repetido')
+        } else {
+            resolve(aleatorio)
+        }
+    })
+}
+
+async function gerarMegaSena(qtdeNumeros) {
+    try {
+        const numeros = []
+        for(let _ of Array(qtdeNumeros).fill()) {
+            numeros.push(await gerarNumerosEntre(1, 60, numeros))
+        }
+        return numeros
+    } catch(e) {
+        throw 'Que chato !!!'
+    }
+}
+
+gerarMegaSena(8)
+    .then(console.log)
+    .catch(console.log)
